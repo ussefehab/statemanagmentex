f1b6b0c7ec93a730ba8f8dc5b365c7f781b7d517
youssefehab25 <123908594+youssefehab25@users.noreply.github.com>
2023-12-24 15:01:21 +0200

    st2

--- lib/check.dart
+++ lib/check.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:statemt/model/cart.dart';
 
-class CheckOut extends StatefulWidget {
+class CheckOut extends StatelessWidget {
   const CheckOut({super.key});
 
-  @override
-  State<CheckOut> createState() => _CheckOutState();
-}
-
-class _CheckOutState extends State<CheckOut> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
